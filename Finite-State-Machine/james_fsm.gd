a018371f389e658f14c12a13410742fb82254bf3
JSVickers <135228503+JSVickers@users.noreply.github.com>
2025-07-28 11:38:55 +0100

    Altered FSM implementation

--- Finite-State-Machine/james_fsm.gd
+++ Finite-State-Machine/james_fsm.gd
@@ -2,22 +2,25 @@
 class_name JamFSM
 extends Node
 
-const EMPTY_STATE_NAME := 'NONE'
+const EMPTY_STATE := -1
 
 signal event_triggered(event_id)
 signal state_exited(state_name)
 signal state_entered(state_name)
 
-var state_dict: Dictionary[String, JamState]
-var trans_dict: Dictionary[String, Dictionary]		#Inner dictionary is [Event_ID: To_State]
-var current_state_name: String = EMPTY_STATE_NAME
-var start_state_name: String = EMPTY_STATE_NAME
+var states : Array[JamState]
+var transitions: Array[Dictionary]
+var current_state: int = EMPTY_STATE
+var start_state: int = EMPTY_STATE
 var shared_data: Variant = null
+var state_ids: Dictionary[String, int]
 
 class JamState extends RefCounted:
+	var state_name: String = ''
 	var state_machine: JamFSM = null
 	var target_node_2d: Node2D = null
 	var target_node_3d: Node3D = null
+	var state_id: int = -1
 	func _init(in_state_machine: JamFSM, in_target_node: Node) -> void:
 		if in_target_node is Node3D:
 			target_node_3d = in_target_node
@@ -32,35 +35,48 @@ class JamState extends RefCounted:
 		pass
 
 func _on_event_triggered(event_id: int):
-	if trans_dict[current_state_name].has(event_id):
-		transition(trans_dict[current_state_name][event_id])
+	if transitions[current_state].has(event_id):
+		transition(transitions[current_state][event_id])
 
-func _set_current_state(new_state_unique_name: String):
-	if current_state_name != EMPTY_STATE_NAME:
-		state_dict[current_state_name].exit()
-		state_exited.emit(current_state_name)
-	current_state_name = new_state_unique_name
-	state_dict[current_state_name].enter()
-	state_entered.emit(current_state_name)
+func _set_current_state(new_state: int):
+	if current_state != EMPTY_STATE:
+		states[current_state].exit()
+		state_exited.emit(current_state)
+	current_state = new_state
+	states[current_state].enter()
+	state_entered.emit(current_state)
 
 func _ready() -> void:
 	event_triggered.connect(_on_event_triggered)
-	_set_current_state(start_state_name)
+	_set_current_state(start_state)
 
-func add_state(state_unique_name: String, state_object: JamState, is_start_state := false):
-	if state_dict.has(state_unique_name):
-		print_debug('State ' + state_unique_name + ' already exists in state machine')
+func add_state(unique_state_name: String, state_object: JamState):
+	var name_already_used = state_ids.has(unique_state_name)
+	assert(!name_already_used, 'State ID is already in use in state machine.')
+	if name_already_used:
 		return
-	state_dict[state_unique_name] = state_object
-	trans_dict[state_unique_name] = {}
-	if is_start_state or start_state_name == EMPTY_STATE_NAME:
-		start_state_name = state_unique_name
+	state_object.state_id = states.size()
+	state_object.state_name = unique_state_name
+	state_ids[unique_state_name] = states.size()
+	states.append(state_object)
+	transitions.append({})
+	if start_state == EMPTY_STATE:
+		start_state = states.size()
 
-func update_state(state_unique_name: String, state_object: JamState):
-	if !state_dict.has(state_unique_name):
-		print_debug('State ' + state_unique_name + ' does not exist in state machine')
-		return
-	state_dict[state_unique_name] = state_object
+func set_start_state(state_id: int):
+	var state_is_valid := state_id < states.size() and state_id > -1
+	assert(state_is_valid, 'State ' + str(state_id) + ' is an invalid start state.')
+	if state_is_valid:
+		start_state = state_id
+
+func get_current_state() -> JamState:
+	return states[current_state]
+
+func update_state(state_id: int, new_state_object: JamState):
+	var state_is_valid := state_id < states.size() and state_id > -1
+	assert(state_is_valid, 'State ' + str(state_id) + ': ' + str(new_state_object) + ' is invalid.')
+	if state_is_valid:
+		states[state_id] = new_state_object
 
 func set_shared_data(new_shared_data: Variant):
 	shared_data = new_shared_data
@@ -71,20 +87,24 @@ func get_shared_data() -> Variant:
 		return null
 	return shared_data
 
-func add_transition(state_from_unique_name: String, event_id: int, state_to_unique_name: String):
-	if trans_dict.has(state_from_unique_name):
-		if trans_dict[state_from_unique_name].has(event_id):
-			print_debug('State ' + state_from_unique_name + ' already has a transition for event, overwriting.')
-	trans_dict[state_from_unique_name][event_id] = state_to_unique_name
+func get_state_id(state_unique_name: String) -> int:
+	if state_ids.has(state_unique_name):
+		return state_ids[state_unique_name]
+	return -1
+
+func add_transition(state_from: int, state_to: int, event: int):
+	if transitions[state_from].has(event):
+		print_debug('State ' + str(state_from) + ' already has a transition for event, overwriting.')
+	transitions[state_from][event] = state_to
 
 func tick(delta: float) -> void:		#Must be called from the parent node
-	state_dict[current_state_name].update(delta)
+	states[current_state].update(delta)
 
-func transition(new_state_unique_name: String):		#Move to another state
-	if !state_dict.has(new_state_unique_name):
-		print_debug('State ' + new_state_unique_name + ' does not exist in state machine')
+func transition(new_state: int):		#Move to another state
+	if states[new_state] == null:
+		print_debug('State ' + str(new_state) + ' does not exist in state machine')
 		return
-	_set_current_state(new_state_unique_name)
+	_set_current_state(new_state)
 
 func trigger_event(event_id: int):
 	event_triggered.emit(event_id)
--- Finite-State-Machine/james_fsm_example_usage.gd
+++ Finite-State-Machine/james_fsm_example_usage.gd
@@ -14,16 +14,6 @@ enum Events{
 	MOUSE_EXITED,
 }
 
-enum ExampleState{
-	COUNT_UP,
-	COUNT_DOWN,
-}
-
-var states: Dictionary[ExampleState, String] = {
-	ExampleState.COUNT_UP: 'Count Up',
-	ExampleState.COUNT_DOWN: 'Count Down',
-}
-
 class ExampleSharedData extends RefCounted:
 	var label_to_change: Label
 	var distance_to_detect: float = 200.0
@@ -31,54 +21,51 @@ class ExampleSharedData extends RefCounted:
 		label_to_change = in_label
 		distance_to_detect = in_distance
 
-class CountingUpState extends JamFSM.JamState:
-	var current_time := 0.0
-	var detection_distance: float
-	var label: Label
+class ExampleBaseState extends JamFSM.JamState:
+	var shared_data: ExampleSharedData
 	func _init(sm: JamFSM, target: Node) -> void:
 		super(sm, target)
-		detection_distance = sm.get_shared_data().distance_to_detect
-		label = sm.get_shared_data().label_to_change
+		shared_data = sm.get_shared_data() as ExampleSharedData
+
+class CountingUpState extends ExampleBaseState:
+	var current_time := 0.0
 	func update(delta: float):
 		current_time += delta
-		label.text = str(snappedf(current_time, 0.1))
+		shared_data.label_to_change.text = str(snappedf(current_time, 0.1))
 		var mouse_distance := target_node_2d.global_position.distance_to(target_node_2d.get_viewport().get_mouse_position())
-		if mouse_distance <= detection_distance:
+		if mouse_distance <= shared_data.distance_to_detect:
 			return state_machine.trigger_event(Events.MOUSE_ENTERED)
 
-class CountingDownState extends JamFSM.JamState:
+class CountingDownState extends ExampleBaseState:
 	var current_time := 0.0
 	var count_from := 30.0
-	var detection_distance: float
-	var label: Label
 	func _init(sm: JamFSM, target: Node, in_count_from: float) -> void:
 		super(sm, target)
 		count_from = in_count_from
-		detection_distance = sm.get_shared_data().distance_to_detect
-		label = sm.get_shared_data().label_to_change
 	func enter():
 		current_time = 0.0
 	func update(delta: float):
 		current_time += delta
 		var remaining := count_from - current_time
-		label.text = str(snappedf(remaining, 0.1))
+		shared_data.label_to_change.text = str(snappedf(remaining, 0.1))
 		if remaining <= 0:
 			return state_machine.trigger_event(Events.TIME_ELAPSED)
 		var mouse_distance := target_node_2d.global_position.distance_to(target_node_2d.get_viewport().get_mouse_position())
-		if mouse_distance > detection_distance:
+		if mouse_distance > shared_data.distance_to_detect:
 			return state_machine.trigger_event(Events.MOUSE_EXITED)
 
 func _ready() -> void:
 	var shared_state_data: ExampleSharedData = ExampleSharedData.new(label, detection_distance)
 	state_machine = JamFSM.new()
 	state_machine.set_shared_data(shared_state_data)
-	var count_up_state := CountingUpState.new(state_machine, marker)
-	var count_down_state := CountingDownState.new(state_machine, marker, count_down_from)
-	state_machine.add_state(states[ExampleState.COUNT_UP], count_up_state, true)
-	state_machine.add_state(states[ExampleState.COUNT_DOWN], count_down_state)
-	state_machine.add_transition(states[ExampleState.COUNT_UP], Events.MOUSE_ENTERED, states[ExampleState.COUNT_DOWN])
-	state_machine.add_transition(states[ExampleState.COUNT_DOWN], Events.MOUSE_EXITED, states[ExampleState.COUNT_UP])
-	state_machine.add_transition(states[ExampleState.COUNT_DOWN], Events.TIME_ELAPSED, states[ExampleState.COUNT_UP])
+	var count_up := CountingUpState.new(state_machine, marker)
+	var count_down := CountingDownState.new(state_machine, marker, count_down_from)
+	state_machine.add_state('Count Up', count_up)
+	state_machine.add_state('Count Down', count_down)
+	state_machine.set_start_state(count_up.state_id)
+	state_machine.add_transition(count_up.state_id, count_down.state_id, Events.MOUSE_ENTERED)
+	state_machine.add_transition(count_down.state_id, count_up.state_id, Events.MOUSE_EXITED)
+	state_machine.add_transition(count_down.state_id, count_up.state_id, Events.TIME_ELAPSED)
 	add_child(state_machine)
 
 func _physics_process(delta: float) -> void:
